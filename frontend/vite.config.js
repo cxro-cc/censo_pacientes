228b7c585bd7439119f8135901b1989fddca4768
unknown <Soporte@GEC-L2-CQQD.opdib.gob.mx>
2026-04-22 12:23:49 -0600

    vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   
+  // Asegurar que Vite interpole las variables de entorno correctamente
+  define: {
+    __VITE_API_BASE_URL__: JSON.stringify(process.env.VITE_API_BASE_URL || 'http://localhost:8000')
+  },
+  
   preview: {
     allowedHosts: [
       "censo-frontend-production-dab7.up.railway.app"
